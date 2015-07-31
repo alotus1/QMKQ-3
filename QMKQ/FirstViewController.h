ba1ab1697356af2693f99468c01a7d845a473f22
skinner_shang <shangjinnet@foxmail.com>
2015-07-31 20:48:56 +0800

    Initial Commit

---
+++ QMKQ/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  QMKQ
+//
+//  Created by shangjin on 15/7/31.
+//  Copyright (c) 2015年 skinner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ QMKQ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QMKQ
+//
+//  Created by shangjin on 15/7/31.
+//  Copyright (c) 2015年 skinner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
